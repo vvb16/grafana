d730b856a3569d24c32bb20f939909f66e9ad4da
Karl Persson <kalle.persson@grafana.com>
2021-12-29 14:11:11 +0100

    Add hooks for setting permissions for dashboards

--- pkg/services/accesscontrol/resourcepermissions/services.go
+++ pkg/services/accesscontrol/resourcepermissions/services.go
@@ -3,6 +3,9 @@ package resourcepermissions
 import (
 	"context"
 	"strconv"
+	"time"
+
+	"github.com/grafana/grafana/pkg/models"
 
 	"github.com/grafana/grafana/pkg/api/routing"
 	"github.com/grafana/grafana/pkg/services/accesscontrol"
@@ -27,6 +30,7 @@ func (s *Services) GetDashboardService() *Service {
 }
 
 func provideDashboardService(sql *sqlstore.SQLStore, router routing.RouteRegister, ac accesscontrol.AccessControl, store accesscontrol.ResourcePermissionsStore) (*Service, error) {
+
 	options := Options{
 		Resource: "dashboards",
 		ResourceValidator: func(ctx context.Context, orgID int64, resourceID string) error {
@@ -50,13 +54,81 @@ func provideDashboardService(sql *sqlstore.SQLStore, router routing.RouteRegiste
 			"Edit":  {"dashboards:read", "dashboards:write", "dashboards:delete"},
 			"Admin": {"dashboards:read", "dashboards:write", "dashboards:delete", "dashboards.permissions:read", "dashboards.permissions:write"},
 		},
-		ReaderRoleName:   "Dashboard permission reader",
-		WriterRoleName:   "Dashboard permission writer",
-		RoleGroup:        "Dashboards",
-		OnSetUser:        nil,
-		OnSetTeam:        nil,
-		OnSetBuiltInRole: nil,
+		ReaderRoleName: "Dashboard permission reader",
+		WriterRoleName: "Dashboard permission writer",
+		RoleGroup:      "Dashboards",
+		OnSetUser: func(ctx context.Context, orgID, userID int64, resourceID, permission string) error {
+			item := models.DashboardAcl{OrgID: orgID, UserID: userID}
+			return onDashboardPermissionUpdated(ctx, sql, resourceID, item, permission)
+		},
+		OnSetTeam: func(ctx context.Context, orgID, teamID int64, resourceID, permission string) error {
+			item := models.DashboardAcl{OrgID: orgID, TeamID: teamID}
+			return onDashboardPermissionUpdated(ctx, sql, resourceID, item, permission)
+		},
+		OnSetBuiltInRole: func(ctx context.Context, orgID int64, builtInRole, resourceID, permission string) error {
+			role := models.RoleType(builtInRole)
+			item := models.DashboardAcl{OrgID: orgID, Role: &role}
+			return onDashboardPermissionUpdated(ctx, sql, resourceID, item, permission)
+		},
 	}
 
 	return New(options, router, ac, store)
+}
+
+func onDashboardPermissionUpdated(ctx context.Context, store *sqlstore.SQLStore, resourceID string, item models.DashboardAcl, permission string) error {
+	return store.WithTransactionalDbSession(ctx, func(sess *sqlstore.DBSession) error {
+		dashboardID, err := strconv.ParseInt(resourceID, 10, 64)
+		if err != nil {
+			return err
+		}
+
+		item.DashboardID = dashboardID
+		has, err := sess.Get(&item)
+		if err != nil {
+			return err
+		}
+
+		if permission == "" {
+			rawSQL := `
+				DELETE FROM dashboard_acl WHERE id = ?
+			`
+			if _, err := sess.Exec(rawSQL, item.Id); err != nil {
+				return err
+			}
+			return nil
+		}
+
+		item.Updated = time.Now()
+		item.Permission = translateDashboardPermission(permission)
+		if has {
+			rawSQL := `
+				UPDATE dashboard_acl
+				SET permission = ?, updated = ?
+				WHERE id = ?
+			`
+			if _, err := sess.Exec(rawSQL, item.Permission, item.Updated, item.Id); err != nil {
+				return err
+			}
+
+			return nil
+		}
+
+		item.Created = time.Now()
+		sess.Nullable("user_id", "team_id")
+		if _, err := sess.Insert(&item); err != nil {
+			return err
+		}
+
+		return nil
+	})
+}
+
+func translateDashboardPermission(permission string) models.PermissionType {
+	permissionLevel := models.PERMISSION_VIEW
+	if permission == models.PERMISSION_EDIT.String() {
+		permissionLevel = models.PERMISSION_EDIT
+	} else if permission == models.PERMISSION_ADMIN.String() {
+		permissionLevel = models.PERMISSION_ADMIN
+	}
+	return permissionLevel
 }
