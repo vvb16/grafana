d70f3b4b5d9da8afad6d3a54c8c7772d407e18ec
Karl Persson <kalle.persson@grafana.com>
2022-01-19 12:13:50 +0100

    add permissions services

--- pkg/services/guardian/provider.go
+++ pkg/services/guardian/provider.go
@@ -3,20 +3,20 @@ package guardian
 import (
 	"context"
 
-	"github.com/grafana/grafana/pkg/services/accesscontrol"
-	"github.com/grafana/grafana/pkg/services/sqlstore"
-
 	"github.com/grafana/grafana/pkg/models"
+	"github.com/grafana/grafana/pkg/services/accesscontrol"
+	"github.com/grafana/grafana/pkg/services/accesscontrol/resourcepermissions"
+	"github.com/grafana/grafana/pkg/services/sqlstore"
 	"github.com/grafana/grafana/pkg/setting"
 )
 
 type Provider struct{}
 
-func ProvideService(cfg *setting.Cfg, store *sqlstore.SQLStore, ac accesscontrol.AccessControl) *Provider {
+func ProvideService(cfg *setting.Cfg, store *sqlstore.SQLStore, ac accesscontrol.AccessControl, permissionServices *resourcepermissions.Services) *Provider {
 	// TODO: remove this hack
 	if cfg.FeatureToggles["accesscontrol"] {
 		New = func(ctx context.Context, dashId int64, orgId int64, user *models.SignedInUser) DashboardGuardian {
-			return &AccessControlDashboardGuardian{ctx: ctx, dashboardID: dashId, user: user, ac: ac, store: store}
+			return NewAccessControlDashboardGuardian(ctx, dashId, user, store, ac, permissionServices)
 		}
 	}
 	return &Provider{}
